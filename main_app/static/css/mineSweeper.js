1ba888135b93706bd827bb6509d0c8b4878a72c0
ronnepuzondev <116023509+ronnepuzondev@users.noreply.github.com>
2023-01-05 11:53:23 -0800

    MVP with five games

--- main_app/static/css/mineSweeper.js
+++ main_app/static/css/mineSweeper.js
@@ -3,29 +3,103 @@ let isBombsGenerated = false
 let bombBoard = []
 let canClick = true
 let numBombs = 40
-console.log("Test")
 console.log(tiles)
 for (let i = 0; i < 216; i++) {
     bombBoard.push(false)
 }
 tiles.forEach(function (tile) {
-    tile.addEventListener('click', translater)
+    tile.addEventListener('click', clickTile)
 })
 tiles.forEach(function (tile) {
     tile.addEventListener('contextmenu', function (tile) {
-        console.log("clicked")
-        tile.target.className = tile.target.className + " flag"
-        console.log(tile.target.className)
+        if (!tile.target.classList.contains("flag") && !tile.target.classList.contains("opened")) {
+            tile.target.className = tile.target.className + " flag"
+        } else {
+            tile.target.className = tile.target.className.replace('flag', '')
+        }
+        console.log(tile.target.textContent)
     })
 })
 
+function clickTile(tile) {
+
+    if (canClick) {
+        if (tile.target.classList.contains("bomb") && !tile.target.classList.contains("flag")) {
+            const bombs = document.querySelectorAll(".bomb");
+            bombs.forEach(function (bomb) {
+                bomb.style.backgroundColor = "red"
+            })
+            win(false)
+        } else {
+            if (!tile.target.classList.contains("opened") && !tile.target.classList.contains("flag")) {
+                tile.target.className = tile.target.className + " opened"
+                checkForSuroundingBombs(tile.target)
+                if (tile.target.textContent == "") {
+                    clickSourounding(tile.target, true)
+                }
+            }
+            if (!isBombsGenerated) {
+                clickSourounding(tile.target, false)
+                generateBombs()
+                clickSourounding(tile.target, true)
+            }
+            // checkForSuroundingBombs(tile)
+        }
+    }
+    isGameOver()
+}
+
+function clickSourounding(tile, repeat) {
+    let row = tile.classList[2]
+    row = row.substring(3);
+    let col = tile.classList[1]
+    col = col.substring(3);
+    for (let y = -1; y < 2; y++) {
+        for (let x = -1; x < 2; x++) {
+            const rowNum = +row + +y
+            const colNum = +col + +x
+            if (colNum != 0 && colNum != 19 && rowNum != 0 && rowNum != 13) {
+                const rowClass = "row" + rowNum
+                const colClass = "col" + colNum
+                let checkedTile = document.getElementsByClassName(rowClass + " " + colClass)
+                checkForSuroundingBombs(checkedTile[0])
+                if (!checkedTile[0].classList.contains("opened") && !checkedTile[0].classList.contains("flag")) {
+                    checkedTile[0].className = checkedTile[0].className + " opened"
+                }
+                if (repeat && checkedTile[0].textContent == "" && !checkedTile[0].classList.contains("opened")) {
+                    clickSourounding(checkedTile[0], true)
+                }
+                // if (checkForSuroundingBombs(checkedTile[0])[0] == 0 && !checkedTile[0].classList.contains("opened")) {
+                //     blankTile(rowNum, colNum)
+                // }
+            }
+        }
+    }
+}
+
+function clickTile2(tile) {
+    if (tile.classList.contains("bomb") && !tile.classList.contains("flag")) {
+        const bombs = document.querySelectorAll(".bomb");
+        bombs.forEach(function (bomb) {
+            bomb.style.backgroundColor = "red"
+        })
+        win(false)
+    } else if (tile.classList.contains("flag")) {
+        console.log("worked")
+    } else {
+        if (!tile.classList.contains("opened")) {
+            tile.className = tile.className + " opened"
+        }
+        checkForSuroundingBombs(tile)
+    }
+}
+
 function translater(tile) {
     if (canClick) {
-        clickTile(tile.target)
+        clickTile2(tile.target)
         if (!isBombsGenerated) {
             generateBombs()
         }
-
         if (checkForSuroundingBombs(tile.target)[0] == 0) {
             blankTile(checkForSuroundingBombs(tile.target)[1], checkForSuroundingBombs(tile.target)[2])
         }
@@ -33,7 +107,7 @@ function translater(tile) {
     isGameOver()
 }
 
-function winLose(win) {
+function win(win) {
     if (win) {
         document.getElementById("win").innerHTML = "Congrats!!! You Win"
     } else {
@@ -50,25 +124,7 @@ function isGameOver() {
 
     })
     if (gameOver == 216 - numBombs) {
-        winLose(true)
-    }
-}
-
-function clickTile(tile) {
-
-    if (tile.classList.contains("bomb") && !tile.classList.contains("flag")) {
-        const bombs = document.querySelectorAll(".bomb");
-        bombs.forEach(function (bomb) {
-            bomb.style.backgroundColor = "red"
-        })
-        winLose(false)
-    } else if (tile.classList.contains("flag")) {
-        console.log("worked")
-    } else {
-        if (!tile.classList.contains("opened")) {
-            tile.className = tile.className + " opened"
-        }
-        checkForSuroundingBombs(tile)
+        win(true)
     }
 }
 
@@ -114,9 +170,7 @@ function blankTile(row, col) {
                 if (!tile.classList.contains("opened")) {
                     checkedTile[0].className = checkedTile[0].className + " opened"
                 }
-
                 if (checkForSuroundingBombs(checkedTile[0])[0] == 0 && !checkedTile[0].classList.contains("opened")) {
-
                     blankTile(rowNum, colNum)
                 }
             }
